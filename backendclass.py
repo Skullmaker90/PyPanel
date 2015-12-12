87af9a676b87f47b02172c510a172f3c1d321cce
Brandon Geyer-Lopez <bgeyer-lopez@cari.net>
2015-12-11 19:40:21 -0800

    Made key_session and backendURL global, fixed function.login so the id, user and password can be passed to it from outside the class using an instance. Still need to resolve issue with 'responseJSON = r.text()' throwing a Unicode error.

--- backendclass.py
+++ backendclass.py
@@ -2,22 +2,27 @@ import json
 import requests
 requests.packages.urllib3.disable_warnings()
 
-class JSONRequest:
-	"""This class allows you to make requests to the backend server."""
-	key_session = None
-	backendURL = 'https://backend.ibizapi.com:8888/JSON/'
-	def __init__(self): pass
-	
-	def login(self, id, user, pass):
-		requestData = {'account_id': id, 'email': user, 'password': pass}
-		requestURL = backendURL + 'AccountManager/AAA?action=Authenticate'
-		r = requests.post(requestURL, data=json.dumps(requestData), verify=False)
-		responseJSON = r.text()
-		data = json.loads(responseJSON)
-		key_session = data['session_key']
-		
-	def caseList(self, key_session):
-		requestData = {'account_id': '1000', 'session_key': key_session, 'MANY', '100'}
-		requestURL = backendURL + 'Case?action=List'
-		r = requests.post(requestURL, data=json.dumps(requestData), verify=False)
-		casesJson = r.json()
+class JSONRequest(object):
+        """This class allows you to make requests to the backend server."""
+        global key_session
+        global backendURL
+        key_session = None
+        backendURL = 'https://backendbeta.ibizapi.com:8888/JSON/'
+        def __init__(self): pass
+
+        def login(self, id, user, password):
+                self.id = id
+                self.user = user
+                self.password = password
+                requestData = {'account_id': id, 'email': user, 'password': password}
+                requestURL = backendURL + 'AccountManager/AAA?action=Authenticate'
+                r = requests.post(requestURL, data=json.dumps(requestData), verify=False)
+                responseJSON = r.text()
+                data = json.loads(responseJSON)
+                key_session = data['session_key']
+
+        def caseList(self):
+                requestData = {'account_id': '1000', 'session_key': key_session, 'MANY': '100'}
+                requestURL = backendURL + 'Case?action=List'
+                r = requests.post(requestURL, data=json.dumps(requestData), verify=False)
+                casesJson = r.json()
