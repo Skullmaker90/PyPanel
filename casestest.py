4a984d53fb916bb8d9a8328e03070096262cd94b
root <root@pypanel.dev>
2015-12-13 02:00:15 -0800

    Added prompt, got runAuth working.

--- casestest.py
+++ casestest.py
@@ -2,13 +2,17 @@
 import backendclass
 import json
 import requests
+import getpass
 from sys import argv, exit
 requests.packages.urllib3.disable_warnings()
 
-script, account_id, email, password = argv
+# Took out this for now so we don't have to enter pass at line, added prompts
+# script, email, password = argv
 
-auth = False
+# Still testing this, thinking about adding a session_key validation to init.
+# auth = False
 
+# Still in testing
 def caseTest():
         if auth == False:
                 print "Not authenticated!"
@@ -17,21 +21,35 @@ def caseTest():
                 print JSONRequest.caseList()
                 exit(0)
 
-def runAuth(id, user, password):
-        print "Authenticating with ID %s, user %s and password %s" % (id, user, password)
+def runAuth(user, password):
+        print "Authenticating with user %s and password %s" % (user, password)
         request = backendclass.JSONRequest()
-        request.login(id, user, password)
+        request.login(user, password)
         if backendclass.key_session != None:
-                print "Success! The session_key is %s" % key_session
-                auth = True
+                print "Success! The session_key is %s" % backendclass.key_session
+		# Added this just to test the caseboard while we write out a session verification.
+		# board = backendclass.JSONRequest()
+		# print board.caseList()
+                # auth = True
         else:
                 print "Authentication failure!"
                 print
 
 
 def __init__():
-        print "Let's test out authentication and case requests."
-        runAuth(account_id, email, password)
-##      caseTest()
-
+	# Added this check so we can optionally create a new session
+	# we should probably replace this with a session verification
+	check = int(input("What would you like to do? (1 for auth, 2 for casetest): "))
+	if check ==  1:
+		# This works, just need to think about logging out.
+		print "Let's test out authentication and case requests."
+		email = raw_input("Email: ")
+		password = getpass.getpass("Password: ")
+        	runAuth(email, password)
+	elif check == 2:
+		print "Let's grab the caseboard."
+		# Still working on this
+        	caseTest()
+	else:
+		exit(0)
 __init__()
