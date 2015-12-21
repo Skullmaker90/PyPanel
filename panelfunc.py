368aa24dab2af7ed5f568cc49c9f6aca804315db
Anthony Smith <asmith@cari.net>
2015-12-20 23:46:51 -0800

    More friendly names.

--- panelfunc.py
+++ panelfunc.py
@@ -10,30 +10,30 @@ requests.packages.urllib3.disable_warnings()
 # the user back at the prompt screen. This will eventually become the
 # backendfunc class with everything we would need to call for the backend.
 
-def auth_check(a):
-	if a.key == None:
+def auth_check(auth):
+	if auth.key == None:
 		user = raw_input("Email: ")
 		password = getpass.getpass("Password: ")
-		a.login(user, password)
-		print("Your session key is: %s") % (a.key)
+		auth.login(user, password)
+		print("Your session key is: %s") % (auth.key)
 
-def case_board(a, request_data):
-	auth_check(a)
-	request_url = a.backend_url + 'Case?action=List'
-	request_data['session_key'] = a.key
+def case_board(auth, request_data):
+	auth_check(auth)
+	request_url = auth.backend_url + 'Case?action=List'
+	request_data['session_key'] = auth.key
 	r = requests.post(request_url, data=json.dumps(request_data), verify=False)
 	cases_json = r.json()
 
-def menu(a, request_data):
+def menu(auth_object, request_data):
 	running = True
 	while running == True:
 		print("What would you like to do? (1 for Caseboard, 2 for logout, 3 to exit)")
 		choice = int(raw_input())
 		if choice == 1:
-			print case_board(a, request_data)
+			print case_board(auth_object, request_data)
 		elif choice == 2:
 			print("Logging out.")
-			a.logout()
+			auth_object.logout()
 		elif choice == 3:
 			running = False
 		else:
@@ -42,7 +42,7 @@ def menu(a, request_data):
 def main():
 	request_data = {'account_id': '2277', 'MANY': '15'}
 	url = 'https://backendbeta.ibizapi.com:8888/JSON/'
-	a = keyauth.keyauth(url)
-	menu(a, request_data)
+	auth_object = keyauth.keyauth(url)
+	menu(auth_object, request_data)
 
 main()
